643909b6e5722da03234fa181a559fda91ab863a
MaDz <000madz000@gmail.com>
2025-07-03 21:33:15 +0300

    remove removed properties

--- src/types/cms_components/sections/blog_hightlight_section.ts
+++ src/types/cms_components/sections/blog_hightlight_section.ts
@@ -2,7 +2,7 @@ import { BlogHighlightType } from "../types/blog_highlight";
 
 export interface BlogHightlightSection {
     title: string;
-    subtitle?: string | null;
-    text?: string | null;
+    // subtitle?: string | null;
+    // text?: string | null;
     highlights: BlogHighlightType[];
 }
