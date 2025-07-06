284faea1d90bdd585b7894d6bb454a47060ee1bd
MaDz <000madz000@gmail.com>
2025-07-06 20:28:47 +0300

    fixing image optimization error in vercel

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,25 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
-    /* config options here */
     images: {
-        remotePatterns: [new URL(`${process.env.STRAPI_HOST}/uploads/**`)],
-        dangerouslyAllowSVG: true
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: new URL(process.env.STRAPI_HOST).hostname,
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: '**'
+            },
+        ]
     }
+    /* config options here */
 };
 
+if (process.env.NODE_ENV === "development") {
+    nextConfig.images?.remotePatterns?.push(
+        new URL(`${process.env.BACKEND_HOST}/uploads/**`)
+    );
+}
 export default withNextIntl(nextConfig);
